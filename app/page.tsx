6d17a318ef9b57bdf206190b45b2e769534c1c0a
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-06-25 11:37:11 -0600

    Remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,4 @@
-import Link from "next/link";
-import { draftMode } from "next/headers";
-
-
-import Date from "./date";
-import CoverImage from "./cover-image";
-
-
-import { getAllPosts } from "@/lib/api";
-
-
-
 export default async function Page() {
-  const { isEnabled } = draftMode();
-  const allPosts = await getAllPosts(isEnabled);
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
-
   return (
     <>
       This is the homepage
