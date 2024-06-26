bc651a273be209b583b156710206f67d57b89275
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-06-26 11:24:25 -0600

    Deploy bug.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/organisms/footer/Footer';
 export const metadata = {
   title: `Kevin Newcombe`,
   description: `Kevin Newcombe, front-end developer`,
-  metadataBase: new URL(`${process.env.VERCEL_URL}`),
+  metadataBase: new URL(`https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`),
   openGraph: {
     images: [
       { 
