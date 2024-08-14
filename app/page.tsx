d28b2adeb77b145b4f22e2c132d796fac5e816ce
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-08-14 10:23:17 -0600

    Move contact form to front page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { fetchStoryBySlug } from "@/lib/api";
 import { StoryblokComponent } from "@storyblok/react/rsc";
 import { notFound } from 'next/navigation';
+import ContactForm from "@/components/organisms/contactform/ContactForm";
 export default async function Page() {
   const { data } = await fetchStoryBySlug('home');
   if(data.error){
     notFound();
   }
   return (
-      <StoryblokComponent blok={data.story.content} />
+      <>
+        <StoryblokComponent blok={data.story.content} />
+        <ContactForm />
+      </>
   );
 }
 
--- components/organisms/contactform/ContactForm.tsx
+++ components/organisms/contactform/ContactForm.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import type { FormEvent } from "react";
 import { contactUsAction } from '@/lib/utils/contact';
 import { getCaptchaToken } from "@/lib/utils/captcha";
-
+import './contactform.scss';
 /**
  * Contact Form
  */
@@ -25,35 +25,37 @@ const ContactForm: React.FC<{}>= ({}) => {
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
-      { userFeedback && userFeedback.success == false && !isSending ? <strong className={`error-text`}>{ userFeedback.message }</strong> : ''}
-      <fieldset disabled={ isSending }>
-        <label>
-          <span className="labeltext">Name</span>
-          <input type="text" id="name" name="name" required />
-        </label>
+    <section className="contactform">
+      <h2>Contact</h2>
+      <form className="contactform__form" onSubmit={handleSubmit}>
+        { userFeedback && userFeedback.success == false && !isSending ? <strong className={`error-text`}>{ userFeedback.message }</strong> : ''}
+        <fieldset disabled={ isSending }>
+          <label>
+            <span className="labeltext">Name</span>
+            <input type="text" id="name" name="name" required />
+          </label>
+          <label>
+            <span className="labeltext">Email</span>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </label>
 
-        <label>
-          <span className="labeltext">Email</span>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
-        </label>
-
-        <label>
-          <span className="labeltext">Message</span>
-          <textarea
-            id="message"
-            name="message"
-            required
-          />
-        </label>
-        <input type="submit" value={ userFeedback?.success ? 'Sent!' : isSending ? `Sending...` : `Send it!` } />
-      </fieldset>
-    </form>
+          <label>
+            <span className="labeltext">Message</span>
+            <textarea
+              id="message"
+              name="message"
+              required
+            />
+          </label>
+          <input type="submit" value={ userFeedback?.success ? 'Sent!' : isSending ? `Sending...` : `Send it!` } />
+        </fieldset>
+      </form>
+    </section>
   );
 }
 
