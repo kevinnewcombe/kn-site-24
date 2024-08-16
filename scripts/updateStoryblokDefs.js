57949d70f88619fc5227811bc48b13a72bccb17b
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-08-16 11:05:27 -0600

    About to start messing with node scripts

---
+++ scripts/updateStoryblokDefs.js
@@ -0,0 +1,16 @@
+// const StoryblokClient = require('storyblok-js-client')
+// init with access token
+// const Storyblok = new StoryblokClient({
+//   accessToken: 'wANpEQEsMYGOwLxwXQ76Ggtt',
+//   cache: {
+//     clear: 'auto',
+//     type: 'memory'
+//   }
+// })
+
+
+// console.clear();
+const a = process.env.STORYBLOK_ACCESS_TOKEN;
+const b = 'test'
+console.log(process.env);
+
