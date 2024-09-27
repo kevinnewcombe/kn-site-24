7849063142e7bfa22b1119abe8063356b3fed2da
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-09-27 10:23:43 -0600

    Disable system theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ThemeProvider enableSystem={ true }>
+		<ThemeProvider enableSystem={ false }>
 			{children}
 		</ThemeProvider>
 	);
