1f4043cb6975f7bfbeb4af6d72e8466f1c8ab4bd
Kevin Newcombe <kevinjnewcombe@gmail.com>
2025-06-13 09:53:20 -0600

    Removed easter egg

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,10 +90,6 @@ export default function RootLayout({
           <Header />
           <main id="main">{children}</main>
           <Script
-            src="/scripts/console.js"
-            strategy="afterInteractive" // lazyOnload, afterInteractive
-          />
-          <Script
             strategy="beforeInteractive"
             src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}`}
           />
